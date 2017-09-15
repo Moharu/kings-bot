c3fcdebdf0c69318f2178b69f20ee7ed28295339
João Vítor Bianchi Teló <joaotelo.nh@hotmail.com>
2017-09-15 19:37:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// express stuff to keep it alive
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send('its alive')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
 // Load up the discord.js library
 const Discord = require("discord.js")
 
